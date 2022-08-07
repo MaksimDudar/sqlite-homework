fb60e4c8f450479b9eb20517cf0885d3cd1735db
MaksimDudar <maksimdudar1@gmail.com>
2022-08-07 18:44:53 +0300

    step3

--- app.rb
+++ app.rb
@@ -1,2 +1,4 @@
 require 'sqlite3'
-db = SQLite3::Database.new 'barbershop.sqlite'
+db = SQLite3::Database.new 'babershop.sqlite'
+db.execute "INSERT INTO feedback (Email, Message) VALUES ('dd@mail.ru', 'Bye-bye')"
+db.close
